b83ca460bc1f9920e2f236ebfdd8522cf3a574a3
majid <kashefymajid1992@gmail.com>
2022-03-08 16:43:42 +0330

    thursday

--- index.php
+++ index.php
@@ -6,13 +6,19 @@
 require_once 'from.php';
 require_once 'to.php';
 
-$sourceRepoUrl = 'git@github.com:majidkashefy1/from-repo.git';
-$destinationRepoUrl = 'git@gitlab.com:majidkashef1/transport-repo2.git';
-
-$sourceDirName = explode('.', array_reverse(explode('/', $sourceRepoUrl))[0])[0];
-
+//$sourceRepoUrl = 'git@github.com:majidkashefy1/from-repo.git';
 $output = null;
 $retval = null;
+
+//$sourceRepoUrl = 'http://172.20.1.5/ir.simorgh.git/AutoWindowsUpdateDisabler.git';
+$sourceRepoUrl = 'git@github.com:majidkashefy1/from-repo.git';
+$sourceDirName = explode('.', array_reverse(explode('/', $sourceRepoUrl))[0])[0];
+
+exec('curl -H "Content-Type:application/json" https://gitlab.com/api/v4/projects?private_token=glpat-FGakNP5qQeAB7dyzXimx -d "{ \"name\": \"'.$sourceDirName.'\" }"');
+
+$destinationRepoUrl = 'git@gitlab.com:kashefsimorgh/'.$sourceDirName.'.git';
+$randString= generateRandomString();
+
 
 //clone repo
 //exec('git clone '.$sourceRepoUrl.' || exit 1', $output, $retval);
@@ -20,13 +26,14 @@ if (!file_exists(__DIR__ . DIRECTORY_SEPARATOR . $sourceDirName)) {
     exec('git clone ' . $sourceRepoUrl);
 }
 
-exec('cd ' . $sourceDirName . ' && git checkout develop && git pull origin develop');
-//exec('git remote add b ' . $destinationRepoUrl, $output, $retval);
-exec('git add . && git commit -m "from1 php"');
-exec('git push b develop', $output, $retval);
-var_dump($output, $retval);
-die;
+//rmdir(__DIR__ . DIRECTORY_SEPARATOR . $sourceDirName);
+//die('cd ' . $sourceDirName. ' && git remote add '.$randString .' '. $destinationRepoUrl. ' && git push '.$randString.' --tags "refs/remotes/origin/*:refs/heads/*"');
+//exec('cd ' . $sourceDirName);
+//exec('git push origin && git checkout develop && git pull origin main');
+//exec('git remote add b ' . $destinationRepoUrl. ' || git status', $output, $retval);
+exec('cd ' . $sourceDirName. ' && git remote add '.$randString .' '. $destinationRepoUrl. ' && git push '.$randString.' --tags "refs/remotes/origin/*:refs/heads/*"', $output, $retval);
 
-echo "Returned with status $retval and output:\n";
-//echo 'done.';
-print_r($output);
+
+var_dump($output, $retval);
+
+//unlink(__DIR__ . DIRECTORY_SEPARATOR . $sourceDirName);
